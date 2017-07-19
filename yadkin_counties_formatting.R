a25d2588ae66ec84aea419aee35742d28729bb27
Sheila <sheilasaia@users.noreply.github.com>
2017-07-18 23:43:56 -0400

    minor header edits

--- yadkin_counties_formatting.R
+++ yadkin_counties_formatting.R
@@ -10,11 +10,14 @@ library(stringr)
 # set directory yadkin counties
 setwd("/Users/ssaia/Documents/sociohydro_project/swat_yadkin_counties/")
 
-# download WPCOMP data
+# load in data
 yadkin_county_data=read_csv("yadkin_counties.txt",col_names=TRUE)
 
 # make new df with just county names
 my_counties=yadkin_county_data %>% select(NAME)
+
+# make all county entries uppercase
+my_counties$NAME=toupper(my_counties$NAME)
 
 # export my_counties
 write_csv(my_counties,"yadkin_counties_sel.csv")
