79bfedd158b77b1067ecc7afa72903aa08ae2d4d
Sheila <sheilasaia@users.noreply.github.com>
2017-07-26 14:17:37 -0400

    reformating and added yadkin counties selection

--- w_and_p_data_formatting.R
+++ w_and_p_data_formatting.R
@@ -2,11 +2,14 @@
 
 # ---- 1. set up ----
 
+# clear workspace
+rm(list = ls())
+
 # load libraries
 library(tidyverse)
 
 
-# ---- 2. reformat WPCOMP data for nc ----
+# ---- 2. reformat WPCOMP data for yadkin counties ----
 
 # set directory for WPCOMP data
 setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/WPCOMP")
@@ -31,7 +34,16 @@ comp_nc_county_data=comp_data %>% filter(COUNTY>37000&COUNTY<37200) # north caro
 # fix name column of comp_nc_data to remove comma
 comp_nc_county_data$NAME=gsub(', NC','',comp_nc_county_data$NAME)
 
-# 
+# bring in info on yadkin
+# set working directory to save data to
+setwd("/Users/ssaia/Documents/sociohydro_project/swat_yadkin_counties/")
+
+# read in yadkin county names/numbers
+yadkin_counties_sel=read_csv("yadkin_counties_sel.csv",col_names=TRUE)
+yadkin_counties_sel_simp=arrange(yadkin_counties_sel,COUNTY) %>% select(-COUNTY)
+
+# select only counties in yadkin ws
+comp_yadkin_data=left_join(yadkin_counties_sel_simp,comp_nc_county_data,by="NAME")
 
 # set working directory to save data to
 setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/reformatted_data")
@@ -41,12 +53,12 @@ write_csv(comp_usa_data,"comp_usa_data.csv")
 write_csv(comp_se_data,"comp_se_data.csv")
 write_csv(comp_nc_data,"comp_nc_data.csv")
 write_csv(comp_nc_county_data,"comp_nc_county_data.csv")
+write_csv(comp_yadkin_data,"comp_yadkin_data.csv")
 
 
-# ---- 3. reformatting WPGEO data for nc ----
+# ---- 3. reformatting WPGEO data for yadkin counties ----
 
 # bring in info on yadkin
-
 # set working directory to save data to
 setwd("/Users/ssaia/Documents/sociohydro_project/swat_yadkin_counties/")
 
@@ -55,7 +67,7 @@ yadkin_counties_sel=read_csv("yadkin_counties_sel.csv",col_names=TRUE)
 yadkin_counties_sel=arrange(yadkin_counties_sel,COUNTY)
 
 # go back to WPGEO data
-setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/WPGEO")
+setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/reformatted_data")
 
 # load in row descriptions to be updated
 my_vars=read_csv("WPGEO_row_rename.csv",col_names=TRUE,col_types=cols(.default = "c")) %>% select(var_name)
@@ -66,7 +78,7 @@ setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/WPGEO/NC"
 # import and edit only yadkin files from WPGEO nc folder
 my_file_paths=paste("WP5",yadkin_counties_sel$COUNTY,".CSV",sep="")
 my_col_names=c("var_name",paste("yr_",seq(1970,2000,5),sep=""),paste("yr_",seq(2001,2020,1),sep=""),paste("yr_",seq(2025,2040,5),sep=""))
-yadkin_geo_data=data.frame()
+geo_yadkin_data=data.frame()
 for (i in 1:length(yadkin_counties_sel$NAME))
   {
     if (i==1) {
@@ -80,7 +92,7 @@ for (i in 1:length(yadkin_counties_sel$NAME))
               COUNTY=rep(yadkin_counties_sel$COUNTY[i],length(my_vars))) %>%
         select(var_name,NAME,COUNTY,yr_2000:yr_2009)
       assign(yadkin_counties_sel$NAME[i],my_table_final)
-      yadkin_geo_data=my_table_final
+      geo_yadkin_data=my_table_final
     }
     
     else {
@@ -94,7 +106,7 @@ for (i in 1:length(yadkin_counties_sel$NAME))
                COUNTY=rep(yadkin_counties_sel$COUNTY[i],length(my_vars))) %>%
         select(var_name,NAME,COUNTY,yr_2000:yr_2009)
       assign(yadkin_counties_sel$NAME[i],my_table_final)
-      yadkin_geo_data=bind_rows(yadkin_geo_data,eval(as.name(yadkin_counties_sel$NAME[i])))
+      geo_yadkin_data=bind_rows(geo_yadkin_data,eval(as.name(yadkin_counties_sel$NAME[i])))
     }
   }
 
@@ -102,4 +114,4 @@ for (i in 1:length(yadkin_counties_sel$NAME))
 setwd("/Users/ssaia/Documents/sociohydro_project/data/woods_pool_v2011/reformatted_data")
 
 # export data
-write_csv(yadkin_geo_data,"yadkin_geo_data.csv")
+write_csv(geo_yadkin_data,"geo_yadkin_data.csv")
