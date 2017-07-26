e7853c8d10b41e52bf4347c9d906e9f3b566883c
Sheila <sheilasaia@users.noreply.github.com>
2017-07-26 13:06:38 -0400

    reformatted script

--- yadkin_counties_formatting.R
+++ yadkin_counties_formatting.R
@@ -1,11 +1,13 @@
 # r script for yadkin counties data formatting
 
+# ---- 1. set up ----
+
 # load libraries
 library(dplyr)
 library(readr)
 library(stringr)
 
-#---- WPCOMP data ----
+# ---- 2. reformat county data yadkin ----
 
 # set directory yadkin counties
 setwd("/Users/ssaia/Documents/sociohydro_project/swat_yadkin_counties/")
@@ -14,7 +16,8 @@ setwd("/Users/ssaia/Documents/sociohydro_project/swat_yadkin_counties/")
 yadkin_county_data=read_csv("yadkin_counties.txt",col_names=TRUE)
 
 # make new df with just county names
-my_counties=yadkin_county_data %>% select(NAME)
+my_counties=yadkin_county_data %>% select(NAME,GEOID) %>%
+  mutate(COUNTY=GEOID) %>% select(-GEOID)
 
 # make all county entries uppercase
 my_counties$NAME=toupper(my_counties$NAME)
