dff3612e647d1033e4b6658d1cc936936a317797
Sheila <sheilasaia@users.noreply.github.com>
2017-08-22 17:00:37 -0400

    started low frequency analysis

--- drought_freq_analysis.R
+++ drought_freq_analysis.R
@@ -14,7 +14,6 @@ library(sf)
 
 # baseline data & river network
 setwd("/Users/ssaia/Documents/sociohydro_project/raw_data/kelly_results/baseline82-08_daily")
-#baseline_sub_data_raw=read_table2("output.sub",col_names=FALSE,skip=9) # baseline .sub file from SWAT
 baseline_rch_data_raw=read_table2("output.rch",col_names=FALSE,skip=9) # basline .rch file from SWAT
 #yadkin_net_data_raw=read_csv("rch_table.txt",col_names=TRUE) # wdreach.shp attribute table from ArcSWAT
 
@@ -42,11 +41,6 @@ yadkin_subs_shp=st_read("subs1.shp",quiet=TRUE)
 # ---- 2. reformat data ----
 
 # column names
-sub_col_names=c("FILE","SUB","GIS","MO","DA","YR","AREAkm2","PRECIPmm","SNOMELTmm",
-                "PETmm","ETmm","SWmm","PERCmm","SURQmm","GWQmm","WYLDmm","SYLDt_ha",
-                "ORGNkg_ha","ORGPkg_ha","NSURQkg_ha","SOLPkg_ha","SEDPkg_ha","LATQmm",
-                "LATNO3kg_ha,GWNO3kg_ha","CHOLAmic_l","CBODUmg_l","DOXQmg_l","TNO3kg_ha",
-                "UNKNOWN","SUB_DUPLICATE")
 rch_col_names=c("FILE","RCH","GIS","MO","DA","YR","AREAkm2","FLOW_INcms","FLOW_OUTcms","EVAPcms",
                 "TLOSScms","SED_INtons","SED_OUTtons","SEDCONCmg_kg","ORGN_INkg","ORGN_OUTkg",
                 "ORGP_INkg", "ORGP_OUTkg","NO3_INkg","NO3_OUTkg","NH4_INkg","NH4_OUTkg",
@@ -58,7 +52,6 @@ rch_col_names=c("FILE","RCH","GIS","MO","DA","YR","AREAkm2","FLOW_INcms","FLOW_O
                 "TOTNkg","TOTPkg","NO3_mg_l","WTMPdegc")
 
 # reassign column names
-colnames(baseline_sub_data_raw)=sub_col_names
 colnames(baseline_rch_data_raw)=rch_col_names
 colnames(csiro4_5_rch_data_raw)=rch_col_names
 colnames(csiro8_5_rch_data_raw)=rch_col_names
@@ -66,10 +59,43 @@ colnames(hadley4_5_rch_data_raw)=rch_col_names
 colnames(miroc8_5_rch_data_raw)=rch_col_names
 
 # remove unnecessary columns
-baseline_sub_data=baseline_sub_data_raw %>% select(SUB,MO:WYLDmm)
 baseline_rch_data=baseline_rch_data_raw %>% select(RCH,MO:FLOW_OUTcms) %>%
   mutate(SUB=RCH) # add column so can join later if needed
 
+
+# ---- 3. function: observation 1-day, low-flow freq analysis (one subbasin) ----
+
+# define function
+obs_rch_lowflow_freq_calcs=function(rch_data) { 
+  # rch_data is df with all reach data for ONLY 1 subbasin
+  
+  # calculate number of years
+  num_yrs=length(unique(rch_data$YR))
+  
+  # find max, sort descending, and adjust
+  obs_df_temp=rch_data %>% 
+    group_by(SUB,YR) %>% 
+    summarise(obs_min_flow_cms=min(FLOW_OUTcms)) %>%
+    arrange(SUB,obs_min_flow_cms) %>%
+    mutate(obs_min_flow_cms_adj=obs_min_flow_cms) %>% # adjust using standard window shift
+    mutate(obs_min_flow_cms_adj_log=log(obs_min_flow_cms_adj))
+  
+  # rank data
+  obs_df_temp$obs_rank_num=seq(1,num_yrs,1)
+  
+  # define return period
+  obs_df_temp$obs_return_period=(num_yrs+1)/obs_df_temp$obs_rank_num
+  
+  # select only necessary fields
+  obs_df=obs_df_temp %>% select(SUB,obs_return_period,obs_min_flow_cms_adj) %>%
+    mutate(data_type=rep("obs",num_yrs))
+  
+  # return
+  return(obs_df)
+}
+
+baseline_rch_data_sel=baseline_rch_data %>% filter(RCH==1)
+baseline_obs_calcs=obs_rch_lowflow_freq_calcs(baseline_rch_data_sel)
 
 
 
@@ -77,8 +103,11 @@ baseline_rch_data=baseline_rch_data_raw %>% select(RCH,MO:FLOW_OUTcms) %>%
 
 # ---- 3. zero stream flow ----
 
+#see methods here: https://pubs.usgs.gov/sir/2008/5126/section3.html
+#see riggs 1972: https://pubs.usgs.gov/twri/twri4b1/pdf/twri_4-B1_a.pdf
+
 # find days where flow_out equals zero
-zero_flow=baseline_rch_data %>% filter(FLOW_OUTcms==0)
+zero_flow=baseline_rch_data %>% filter(FLOW_OUTcms<0.01)
 
 # count number of days for each basin (for each year) where flow equals zero
 zero_flow_tally=zero_flow %>% group_by(SUB,YR) %>% 
