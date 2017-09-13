55ee1bbfa5567525558186b3d63ccaa02b602b74
Sheila <sheilasaia@users.noreply.github.com>
2017-09-13 15:48:19 -0400

    remove outliers and high model values

--- functions/model_lowflow_freq_calcs_one_rch.R
+++ functions/model_lowflow_freq_calcs_one_rch.R
@@ -21,18 +21,17 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
   # https://water.usgs.gov/osw/TRB/Bulletin17B_Computations_TRB_010909.pdf
   
   # save temporary variables
-  current_sub=unique(obs_lowflow_freq_calcs_df$RCH)
+  current_rch=unique(obs_lowflow_freq_calcs_one_rch_df$RCH)
   num_p=length(model_p_list)
-  num_yrs=dim(obs_lowflow_freq_calcs_df)[1]
-  obs_rank_num=obs_lowflow_freq_calcs_df$obs_rank_num
-  obs_return_period=obs_lowflow_freq_calcs_df$obs_return_period_yr
-  obs_flow_unlog=obs_lowflow_freq_calcs_df$obs_min_flow_cms_adj
+  num_yrs=dim(obs_lowflow_freq_calcs_one_rch_df)[1]
+  obs_return_period=obs_lowflow_freq_calcs_one_rch_df$obs_return_period_yr
+  obs_flow_unlog=obs_lowflow_freq_calcs_one_rch_df$obs_min_flow_cms_adj
   
-  # just data > zero
-  obs_overzero_rank_num=obs_rank_num[obs_flow_unlog>0] # select only data > zero
-  obs_overzero_return_period=obs_return_period[obs_flow_unlog>0]
-  obs_overzero_flow_unlog=obs_flow_unlog[obs_flow_unlog>0]
-  num_overzero_yrs=length(obs_overzero_flow_unlog)
+  # save data frame with data > zero
+  obs_overzero_data=obs_lowflow_freq_calcs_one_rch_df %>% filter(obs_min_flow_cms_adj>0)
+  obs_overzero_return_period=obs_overzero_data$obs_return_period_yr # select only data > zero
+  obs_overzero_flow_unlog=obs_overzero_data$obs_min_flow_cms_adj
+  num_overzero_yrs=dim(obs_overzero_data)[1]
   
   # percent of data = zero
   num_zero_yrs=num_yrs-num_overzero_yrs
@@ -49,8 +48,11 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
     if (perc_zeros_check==0) {
       # low flow analysis without conditional probability adjustment
       
+      # remove outliers
+      obs_temp_data=remove_outliers(obs_lowflow_freq_calcs_one_rch_df,kn_table)
+      
       # log inputs and find mean
-      obs_flow_log=log(obs_flow_unlog)
+      obs_flow_log=obs_temp_data$obs_min_flow_log_cms_adj
       obs_mean=mean(obs_flow_log)
       # calculate difference from mean
       obs_mean_diff=obs_flow_log-obs_mean
@@ -72,7 +74,7 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
       model_flow_unlog=exp(obs_mean+obs_stdev*model_kt)
       
       # final output dataframe
-      model_df=data.frame(RCH=rep(current_sub,num_p),
+      model_df=data.frame(RCH=rep(current_rch,num_p),
                           model_return_period_yr=1/(1-(model_p_list)),
                           model_flow_log_cms=model_flow_log,
                           model_flow_cms=model_flow_unlog,
@@ -86,8 +88,11 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
       
       # if there are any flows=0 then do conditional probability adjustment
       
+      # remove outliers
+      obs_temp_data=remove_outliers(obs_overzero_data,kn_table)
+      
       # log inputs > zero and calculate mean
-      obs_overzero_flow_log=log(obs_overzero_flow_unlog)
+      obs_overzero_flow_log=obs_temp_data$obs_min_flow_log_cms_adj
       obs_overzero_mean=mean(obs_overzero_flow_log)
       
       # calculate standard deviation and coefficient of skew (cskew)
@@ -99,19 +104,6 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
       obs_overzero_variance=(1/(num_yrs-1))*obs_overzero_mean_diff_sqrd_sum
       obs_overzero_stdev=sqrt(obs_overzero_variance)
       obs_overzero_cskew=(num_yrs*obs_overzero_mean_diff_cubed_sum)/((num_yrs-1)*(num_yrs-2)*(obs_overzero_stdev^3))
-      
-      # if skew is between -0.4 and +0.4 then no need to remove outliers, otherwise, need to
-      if (obs_overzero_cskew<-0.4) {
-        obs_flow_low_limit=obs_overzero_mean-kn_value*obs_overzero_stdev
-        #output new df w/in limits
-      }
-      
-      if (obs_overzero_cskew>0.4) {
-        obs_flow_low_limit=obs_overzero_mean+kn_value*obs_overzero_stdev
-        #output new df w/in limits
-      }
-      
-      # calculate flow outliers...threshold pg 136/142
       
       # adj probability
       obs_overzero_prob=1/obs_overzero_return_period
@@ -126,8 +118,8 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
       con_flow_unlog=exp(obs_overzero_mean+obs_overzero_stdev*con_kt)
       con_prob_adj=1-perc_zeros_check
       con_p_list=model_p_list*con_prob_adj
-      con_return_period_no_adj=1/(1-model_p_list)
-      con_return_period_adj=1/(1-con_p_list)
+      #con_return_period_no_adj=1/(1-model_p_list)
+      #con_return_period_adj=1/(1-con_p_list)
       
       # fit smooth and predict
       con_smooth=loess(con_flow_log~con_p_list)
@@ -167,11 +159,12 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
       fin_kt=logpearson3_factor_calc(model_p_list,wtd_cskew)
       
       # final output dataframe
-      model_df=data.frame(RCH=rep(current_sub,num_p),
+      model_df=data.frame(RCH=rep(current_rch,num_p),
                           model_return_period_yr=1/(1-model_p_list),
                           model_flow_log_cms=syn_mean+syn_stdev*fin_kt,
                           model_flow_cms=exp(syn_mean+syn_stdev*fin_kt),
-                          data_type=rep("model",num_p))
+                          data_type=rep("model",num_p)) %>%
+        filter(model_flow_cms<(round(max(obs_lowflow_freq_calcs_one_rch_df$obs_min_flow_cms_adj))+5)) # remove flow estimations beyond observations
       
       # plotting (just to check)
       #plot(obs_flow_unlog~obs_return_period,pch=16)
@@ -191,7 +184,7 @@ model_lowflow_freq_calcs_one_rch=function(obs_lowflow_freq_calcs_one_rch_df,kn_t
     # return df without completing any calculations
     # low flow analysis cannot be done when zeros make up >25% of data
     
-    model_df=data.frame(RCH=rep(current_sub,num_p),
+    model_df=data.frame(RCH=rep(current_rch,num_p),
                         model_return_period_yr=1/(1-(model_p_list)), 
                         model_flow_log_cms=rep(as.numeric("NA"),num_p),
                         model_flow_cms=rep(as.numeric("NA"),num_p),
