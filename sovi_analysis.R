94484bab5c63a6291646479dee05e900e6bb88d6
Sheila <sheilasaia@users.noreply.github.com>
2017-10-13 13:25:47 -0400

    plot wtd sovi data vs flood freq results

--- sovi_analysis.R
+++ sovi_analysis.R
@@ -24,6 +24,13 @@ yadkin_counties_raw=read_csv("yadkin_clip_counties_albers.txt",col_names=TRUE)
 setwd("/Users/ssaia/Documents/ArcGIS/yadkin_arcgis_analysis_albers/county_scaling_calculations")
 county_scaling_raw=read_csv("county_scaling_allsubs.csv",col_names=TRUE)
 
+# flood frequency results
+setwd("/Users/ssaia/Documents/sociohydro_project/analysis/results/r_outputs")
+flood_10yr_data=read_csv("flood_10yr_perc_change.csv",col_names=TRUE)
+flood_100yr_data=read_csv("flood_10yr_perc_change.csv",col_names=TRUE)
+#lowflow_10yr_data=read_csv("lowflow_10yr_perc_change.csv",col_names=TRUE)
+#lowflow_100yr_data=read_csv("lowflow_100yr_perc_change.csv",col_names=TRUE)
+
 # gis data
 # county bounds (.shp file)
 setwd("/Users/ssaia/Documents/ArcGIS/yadkin_arcgis_analysis_albers/")
@@ -71,6 +78,9 @@ us_nc_yadkin_sovi_data=bind_rows(yadkin_sovi_data,nc_sovi_data,us_sovi_data) %>%
 # reformat county scaling so matches other columns in other datasets
 county_scaling_data=county_scaling_raw %>% select(SUB,fip_code=GEOID,area_perc=AREA_PERC)
 
+# select data for yadkin sovi plot
+yadkin_sovi_data_sel=yadkin_sovi_data %>% select(fip_code,county_sovi,percentile)
+yadkin_counties_shp_sovi=yadkin_counties_shp %>% left_join(yadkin_sovi_data_sel,by="fip_code")
 
 # gis data
 yadkin_subs_shp=yadkin_subs_shp %>% mutate(SUB=Subbasin) # add SUB column to .shp file
@@ -127,16 +137,12 @@ ggplot(us_nc_yadkin_sovi_data,aes(county_sovi,fill=dataset,color=dataset)) +
   coord_cartesian(ylim=c(0,20)) +
   theme_bw()
 
-# plot sovi for yadkin
-yadkin_sovi_data_sel=yadkin_sovi_data %>% select(fip_code,county_sovi)
-yadkin_counties_shp_sovi=yadkin_counties_shp %>% left_join(yadkin_sovi_data_sel,by="fip_code")
-
 #setwd("/Users/ssaia/Desktop")
 #cairo_pdf("yadkin_sovi_by_county.pdf",width=11,height=8.5)
 ggplot(yadkin_counties_shp_sovi,aes(fill=county_sovi)) +
   geom_sf() +
   coord_sf(crs=st_crs(102003)) + # yadkin_counties_shp_sovi is base utm 17N so convert to Albers for CONUS
-  scale_fill_gradient2("SoVI") +
+  scale_fill_gradient2("SoVI",low="blue",high="red") +
   theme_bw()
 #dev.off()
 
@@ -145,22 +151,51 @@ ggplot(yadkin_counties_shp_sovi,aes(fill=county_sovi)) +
 
 # join county scaling data to yadkin sovi select data
 wtd_calcs_data=left_join(yadkin_sovi_data_sel,county_scaling_data,by="fip_code") %>%
-  mutate(area_wtd_sovi_indiv=county_sovi*area_perc) %>%
-  group_by(SUB) %>% summarise(area_wtd_sovi=sum(area_wtd_sovi_indiv))
+  mutate(area_wtd_sovi_indiv=county_sovi*area_perc,area_wtd_percentile_indiv=percentile*area_perc) %>%
+  group_by(SUB) %>% summarise(area_wtd_sovi=sum(area_wtd_sovi_indiv),area_wtd_percentile=sum(area_wtd_percentile_indiv))
 
 # add to shp file
 yadkin_subs_shp_sovi=yadkin_subs_shp %>% left_join(wtd_calcs_data,by="SUB")
 #glimpse(yadkin_subs_shp_sovi)
 
+# question: does it make sense to area wt sovi and sovi percentiles?
 
-# ---- 4.2 plot county data in space ----
+
+# ---- 4.2 plot county sovi data in space ----
 
 #setwd("/Users/ssaia/Desktop")
 #cairo_pdf("yadkin_sovi_by_sub.pdf",width=11,height=8.5)
 ggplot(yadkin_subs_shp_sovi,aes(fill=area_wtd_sovi)) +
   geom_sf() +
   coord_sf(crs=st_crs(102003)) + # yadkin_subs_shp_sovi is base utm 17N so convert to Albers for CONUS
-  scale_fill_gradient2("Area Weighted SoVI") +
+  scale_fill_gradient2("Area Weighted SoVI",low="blue",high="red") +
   theme_bw()
 #dev.off()
 
+#setwd("/Users/ssaia/Desktop")
+#cairo_pdf("yadkin_sovi_percentile_by_sub.pdf",width=11,height=8.5)
+ggplot(yadkin_subs_shp_sovi,aes(fill=area_wtd_percentile)) +
+  geom_sf() +
+  coord_sf(crs=st_crs(102003)) + # yadkin_subs_shp_sovi is base utm 17N so convert to Albers for CONUS
+  scale_fill_gradient2("Area Weighted SoVI Percentile",limits=c(0,100)) +
+  theme_bw()
+#dev.off()
+
+
+# ---- 5.1 compare flood and lowflow freq results to scaled sovi data ----
+
+# join flood and sovi data
+flood_10yr_sovi_data=left_join(flood_10yr_data,wtd_calcs_data,by="SUB") 
+
+# use scale() to get z score
+
+
+# ---- 5.2 plot flood and lowflow freq results to scaled sovi data ----
+
+flood_10yr_sovi_data$dataset=factor(flood_10yr_sovi_data$dataset,levels=c("miroc8_5","csiro8_5","csiro4_5","hadley4_5"))
+ggplot(flood_10yr_sovi_data,(aes(x=area_wtd_sovi,y=perc_change,color=dataset))) +
+  geom_point(size=2) +
+  scale_x_reverse() +
+  xlab("Area Weighted SoVI") +
+  ylab("10yr Flood Frequency % Change (from Baseline)") +
+  theme_bw()
