28c2548d0b0e3691205095fa3eb409b318101bb0
Sheila <sheilasaia@users.noreply.github.com>
2017-10-20 17:51:58 -0400

    change to use nonrescaled percent change in flood frequency

--- functions/risk_vuln_reclass.R
+++ functions/risk_vuln_reclass.R
@@ -6,13 +6,13 @@
 # contact: ssaia [at] ncsu [dot] edu
 
 # define function
-risk_vuln_reclass=function(flood_freq_sovi_data_rescale) {
+risk_vuln_reclass=function(flood_freq_sovi_data) {
   
   # run for each dataset
-  # case 1: if flood freq rescaled is above 0 & sovi rescaled is equal to/below 0 (high risk/low vulnerability)
-  # case 2: if flood freq rescaled is equal to/below 0 & sovi rescaled is equal to/below 0 (low risk/low vulnerability)
-  # case 3: if flood freq rescaled is above 0 & sovi rescaled is above 0 (high risk/high vulnerability)
-  # case 4: if flood freq rescaled is equal to/below 0 & sovi rescaled is above 0 (low risk/high vulnerability)
+  # case 1: if flood freq is above 0 & sovi rescaled is equal to/below 0 (high risk/low vulnerability)
+  # case 2: if flood freq is equal to/below 0 & sovi rescaled is equal to/below 0 (low risk/low vulnerability)
+  # case 3: if flood freq is above 0 & sovi rescaled is above 0 (high risk/high vulnerability)
+  # case 4: if flood freq is equal to/below 0 & sovi rescaled is above 0 (low risk/high vulnerability)
   
   # make output dataframe to hold results
   output_df=data.frame(SUB=as.numeric(),
@@ -20,33 +20,32 @@ risk_vuln_reclass=function(flood_freq_sovi_data_rescale) {
                        perc_change=as.numeric(),
                        area_wtd_sovi=as.numeric(),
                        area_wtd_percentile=as.numeric(),
-                       perc_change_rescale=as.numeric(),
-                       area_wtd_sovi_rescale=as.numeric(),
+                       area_wtd_sovi_rescaled=as.numeric(),
                        sovi_class=as.character())
-  num_datasets=length(unique(flood_10yr_sovi_data_rescale$dataset))
+  num_datasets=length(unique(flood_freq_sovi_data$dataset))
   
   # iterate through data
   for (i in 1:num_datasets) {
     
     # select one dataset for analysis
-    temp_dataset_name=as.character(unique(flood_10yr_sovi_data_rescale$dataset)[i])
-    temp_df=flood_10yr_sovi_data_rescale %>% filter(dataset==temp_dataset_name)
+    temp_dataset_name=as.character(unique(flood_freq_sovi_data$dataset)[i])
+    temp_df=flood_freq_sovi_data %>% filter(dataset==temp_dataset_name)
     temp_sovi_class=as.character()
     
     # classify according to cases 1-4 above
     for (j in 1:dim(temp_df)[1]) {
-      temp_perc_change_rescale=temp_df$perc_change_rescale[j]
-      temp_area_wtd_sovi_rescale=temp_df$area_wtd_sovi_rescale[j]
-      if (temp_perc_change_rescale>0 && temp_area_wtd_sovi_rescale<=0) { #case 1
+      temp_perc_change=temp_df$perc_change[j]
+      temp_area_wtd_sovi_rescaled=temp_df$area_wtd_sovi_rescaled[j]
+      if (temp_perc_change>0 && temp_area_wtd_sovi_rescaled<=0) { #case 1
         temp_sovi_class[j]="hi_risk_lo_vuln"
       }
-      else if (temp_perc_change_rescale<=0 & temp_area_wtd_sovi_rescale<=0) { #case 2
+      else if (temp_perc_change<=0 & temp_area_wtd_sovi_rescaled<=0) { #case 2
         temp_sovi_class[j]="lo_risk_lo_vuln"
       }
-      else if (temp_perc_change_rescale>0 & temp_area_wtd_sovi_rescale>0) { #case 3
+      else if (temp_perc_change>0 & temp_area_wtd_sovi_rescaled>0) { #case 3
         temp_sovi_class[j]="hi_risk_hi_vuln"
       }
-      else if (temp_perc_change_rescale<=0 & temp_area_wtd_sovi_rescale>0) { #case 4
+      else if (temp_perc_change<=0 & temp_area_wtd_sovi_rescaled>0) { #case 4
         temp_sovi_class[j]="lo_risk_hi_vuln"
       }
     }
