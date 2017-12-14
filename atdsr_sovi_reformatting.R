3a9603c9074908c8191f321f6e57abd729886b09
Sheila <sheilasaia@users.noreply.github.com>
2017-12-14 11:21:57 -0500

    fixed error to allow values equal to zero through

--- atdsr_sovi_reformatting.R
+++ atdsr_sovi_reformatting.R
@@ -16,22 +16,22 @@ sovi_raw=read_csv("us_sovi2014_albers.txt",col_names=TRUE,na=c("-999"))
 
 # select only columns you need
 sovi_data=sovi_raw %>%
-  select(fip_code=STCNTY,state_fip=ST,tract_fip=TRACTCE,state_abbrev=ST_ABBR,county_name=COUNTY,
+  select(fips=FIPS,state_abbrev=ST_ABBR,county_name=COUNTY,
          sovi_theme1=SPL_THEME1,sovi_theme1_percentile=RPL_THEME1,
          sovi_theme2=SPL_THEME2,sovi_theme2_percentile=RPL_THEME2,
          sovi_theme3=SPL_THEME3,sovi_theme3_percentile=RPL_THEME3,
          sovi_theme4=SPL_THEME4,sovi_theme4_percentile=RPL_THEME4,
          sovi_total=SPL_THEMES,sovi_total_percentile=RPL_THEMES) %>%
   # remove -999 vaules (=NA)
-  filter(sovi_theme1>0 & sovi_theme1_percentile>0 &
-           sovi_theme2>0 & sovi_theme2_percentile>0 &
-           sovi_theme3>0 & sovi_theme3_percentile>0 &
-           sovi_theme4>0 & sovi_theme4_percentile>0 &
-           sovi_total>0 & sovi_total_percentile>0)
+  filter(sovi_theme1>=0 & sovi_theme1_percentile>=0 &
+           sovi_theme2>=0 & sovi_theme2_percentile>=0 &
+           sovi_theme3>=0 & sovi_theme3_percentile>=0 &
+           sovi_theme4>=0 & sovi_theme4_percentile>=0 &
+           sovi_total>=0 & sovi_total_percentile>=0)
 
 
 # ---- 3 export data ----
 
 # export reformatted data
 setwd("/Users/ssaia/Documents/sociohydro_project/analysis/results/r_outputs")
-write_csv(sovi_data,"atsdr_sovi_data.csv")
+write_csv(sovi_data,"atsdr_us_sovi_data.csv")
